086d4adbad71eb6a953a2e2a0d49030a67e96adb
Gabriel Montoya <gabi.montoya@gmail.com>
2020-12-09 16:39:31 -0300

    Add project files.

---
+++ CourseLibrary.API/Models/CourseForUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Models
+{
+
+    public class CourseForUpdateDto : CourseForManipulationDto
+    {
+        [Required(ErrorMessage = "You should fill out a description.")]
+        public override string Description { get => base.Description; set => base.Description = value; }
+    }
+}
